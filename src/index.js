37dcb89065fd884eb0bb9c65eafc3defbe77e14c
Alex Teisheira <fukirx@gmail.com>
2020-11-24 17:33:59 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import "bootstrap-4-grid";
+import "./sass/fonts.sass";
+import "./sass/reset.sass";
+import "./sass/main.sass";
+import { gsap } from "gsap";
+
+let cx,
+  cy,
+  mouseX,
+  mouseY,
+  posX,
+  posY,
+  clientX,
+  clientY,
+  dx,
+  dy,
+  tiltx,
+  tilty,
+  request,
+  radius,
+  degree;
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Custom JS
+
+  const body = document.querySelector("body");
+
+  cx = window.innerWidth / 2;
+  cy = window.innerHeight / 2;
+
+  body.addEventListener("mousemove", (e) => {
+    clientX = e.pageX;
+    clientY = e.pageY;
+
+    request = requestAnimationFrame(updateMe);
+  });
+
+  function updateMe() {
+    dx = clientX - cx;
+    dy = clientY - cy;
+    tiltx = dy / cy;
+    tilty = dx / cx;
+    radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2));
+    degree = radius * 12;
+    gsap.to(".content", 1, {
+      transform: `rotate3d( ${tiltx}, ${tilty}, 0, ${degree}deg )`,
+    });
+  }
+});
